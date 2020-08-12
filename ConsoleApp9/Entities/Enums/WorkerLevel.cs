213f0edd531564b5df4e4541e8713a546c161666
Matheus Hoesel Dias <matheushoeseldias@gmail.com>
2020-08-12 16:43:44 -0300

    Projeto pronto

---
+++ ConsoleApp9/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9.Entities.Enums
+{
+    enum WorkerLevel
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+
+}
---
+++ ConsoleApp9/Entities/HourContract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+
+        public double ValuePerHour { get; set; }
+
+        public double Hour { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, double hour)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hour = hour;
+        }
+        public double TotalValue()
+        {
+            return ValuePerHour * Hour;
+        }
+    }
+}
